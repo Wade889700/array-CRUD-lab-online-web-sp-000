e3ad5af207a01fa6a93abd8206ba0b3d165ba466
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-17 22:44:17 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,19 +9,18 @@ end
 
 def add_element_to_end_of_array(array, element)
   return array.push(element)
-
 end
 
 def add_element_to_start_of_array(array, element)
-
+  return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  return array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  return array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
